54b366a8c48e8b116f9a45b68038615942cb7e22
Dionis Letnikov <113114648+sssrankpeek@users.noreply.github.com>
2022-09-22 15:13:22 +0300

    Add files via upload

--- dzzklb3/dzzklb3/Source.c
+++ dzzklb3/dzzklb3/Source.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include<locale.h>
 #define a 63241.126
-void main()
+double svgod (int god)
+{
+	double result = a * god;
+	return result;
+}
+
+void main(void)
 {
 	setlocale(LC_ALL, "RUS");
 	int god;
 	double result;
 	puts("введите число световых лет:");
 	scanf("%d",&god);
-	result = a * god;
+	result = svgod(god);
 	printf("%d световых года - это %.3lf а.е.", god, result);
 
 }
